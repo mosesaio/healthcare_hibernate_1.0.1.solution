c960bacda43378296669b9bab7fb36b224d35915
mosesaio <Ayoaade1@gmail.com>
2025-01-28 13:44:36 -0700

    Add all to files

---
+++ src/main/java/healthcaremanagement/model/Doctor.java
@@ -0,0 +1,93 @@
+package healthcaremanagement.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Doctors")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "DoctorID")
+    private int doctorId;
+
+    @Column(name = "FirstName")
+    private String firstName;
+
+    @Column(name = "LastName")
+    private String lastName;
+
+    @Column(name = "Speciality")
+    private String speciality;
+
+    @Column(name = "Email")
+    private String email;
+
+
+
+
+
+    public Doctor() {
+    }
+
+    // Parameterized constructor for convenience
+    public Doctor(String firstName, String lastName, String speciality, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.speciality = speciality;
+        this.email = email;
+    }
+
+
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "doctorId=" + doctorId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", speciality='" + speciality + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
